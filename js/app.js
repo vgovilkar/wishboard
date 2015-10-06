940b200fbd3473acdaaac15165be784895a37f8d
vgovilkar <vaibhav.govilkar@orbitz.com>
2015-10-05 20:53:27 -0500

    trial

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-var app = angular.module("myApp", ["firebase"]);
+var app = angular
+  .module("myApp", ["firebase", "ngRoute"])
+  .config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+    $routeProvider
+    .when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'LoginController',
+    })
+    .when('/main', {
+      templateUrl: 'views/lists.html',
+      controller: 'MainController',
+    })
+    .otherwise({
+      templateUrl: 'views/login.html',
+      controller: 'LoginController',
+    });;
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+  }]);
--- js/controllers/LoginController.js
+++ js/controllers/LoginController.js
@@ -1,11 +1,11 @@
-
-app.controller("LoginController", ["$scope", "Auth",
+app.controller("LoginController", ["$scope", "$location", "Auth",
   function($scope, Auth) {
     $scope.auth = Auth;
 
     // any time auth status updates, add the user data to scope
     $scope.auth.$onAuth(function(authData) {
       $scope.authData = authData;
+      $location.path('/main')
     });
   }
 ]);
